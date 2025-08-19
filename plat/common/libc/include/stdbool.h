eeb36cacfc7fe97b3f941e26b288963964b4be25
techflashYT <officialTechflashYT@gmail.com>
2025-08-18 22:14:57 -0700

    plat: common: libc: add stdbool

---
+++ plat/common/libc/include/stdbool.h
@@ -0,0 +1,14 @@
+/*
+ * EverythingNet - Internal Libc - stdbool
+ * Copyright (C) 2025 Techflash
+ */
+
+#ifndef _STDBOOL_H
+#define _STDBOOL_H
+
+typedef int bool;
+#define true 1
+#define false 0
+
+#endif
+
