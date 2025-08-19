7025da2f2eeaccf92d0218fe25708bf8a12760c0
techflashYT <officialTechflashYT@gmail.com>
2025-08-19 07:44:38 -0700

    plat: nt4: add minimal libc subset from common

---
+++ plat/nt4/libc/include/stdint.h
@@ -0,0 +1,16 @@
+/*
+ * EverythingNet - Internal Libc - stdint
+ * Copyright (C) 2023-2025 Techflash
+ */
+
+typedef char int8_t;
+typedef unsigned char uint8_t;
+
+typedef short int16_t;
+typedef unsigned short uint16_t;
+
+typedef int int32_t;
+typedef unsigned int uint32_t;
+
+typedef long int int64_t;
+typedef unsigned long int uint64_t;
