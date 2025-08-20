a6697135852445605cbd794381c62751a6ab6abb
techflashYT <officialTechflashYT@gmail.com>
2025-08-19 23:12:00 -0700

    nt4: fix PLAT_Init sig, call NET_Init

--- plat/nt4/init.c
+++ plat/nt4/init.c
@@ -5,10 +5,15 @@
 #include <stdlib.h>
 #include <evrnet/state.h>
 #include <evrnet/plat/nt4.h>
+#include <evrnet/net.h>
 
-int PLAT_Init(void) {
+int PLAT_Init(int argc, char *argv[]) {
+	(void)argc;
+	(void)argv;
 	G_State.exitCallbacks.exit = exit;
 
 	NT4_GatherDeviceInfo();
+
+	NET_Init();
 	return 0;
 }
